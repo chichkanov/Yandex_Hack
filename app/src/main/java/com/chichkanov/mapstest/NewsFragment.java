98561f6593e6bb0ec63bdf62119fdc0fd5c1dda8
chichkanov <agchichkanov@gmail.com>
2017-07-08 13:39:56 +0300

    Added rating view

--- app/src/main/java/com/chichkanov/mapstest/NewsFragment.java
+++ app/src/main/java/com/chichkanov/mapstest/NewsFragment.java
@@ -1,9 +1,22 @@
 package com.chichkanov.mapstest;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by chichkanov on 08.07.17.
  */
 
-public class NewsFragment {
+public class NewsFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_map, container, false);
+        return layout;
+    }
 }
