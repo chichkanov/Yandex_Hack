41bdb86c5faffbb752fc7573adac67308b3a26b6
Denis Buzmakov <lysva94@gmail.com>
2017-07-08 16:44:42 +0300

    changes on map

--- app/src/main/java/com/chichkanov/mapstest/MapsFragment.java
+++ app/src/main/java/com/chichkanov/mapstest/MapsFragment.java
@@ -1,20 +1,34 @@
 package com.chichkanov.mapstest;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.chichkanov.mapstest.model.Place;
+import com.chichkanov.mapstest.model.Storage;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
@@ -29,6 +43,13 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MapsFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener {
@@ -36,15 +57,22 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, Google
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
     private GoogleMap mMap;
-    GoogleApiClient mGoogleApiClient;
-    Location mLastLocation;
-    Marker mCurrLocationMarker;
-    LocationRequest mLocationRequest;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private LocationRequest mLocationRequest;
+    private Map<String, Boolean> mCurrentCategories = new LinkedHashMap<>();
+
+    private RecyclerView mRecyclerPlaces;
+    private PlacesAdapter mAdapter;
+    private ImageButton mBtnFilter;
+    private EditText mTxtSearch;
+    private Button mBtnShowList;
+    private BottomSheetBehavior mBottomSheetBehavior;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View layout = inflater.inflate(R.layout.fragment_map, container, false);
+        final View layout = inflater.inflate(R.layout.fragment_map, container, false);
 
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             checkLocationPermission();
@@ -54,12 +82,128 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, Google
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        mTxtSearch = (EditText) layout.findViewById(R.id.txt_search);
+
+        initPlacesList(layout);
+        initFilterButton(layout);
+        initBottomSheet(layout);
+
         return layout;
+    }
+
+    private void initFilterButton(View layout) {
+        mBtnFilter = (ImageButton) layout.findViewById(R.id.btn_filter);
+        final String[] categories = Storage.getInstance(getActivity().getApplicationContext()).getCategories();
+        for(String c : categories) {
+            mCurrentCategories.put(c, true);
+        }
+        mBtnFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+                builder.setTitle(R.string.choose_categories);
+                boolean[] choosedCategories = new boolean[mCurrentCategories.values().size()];
+                for(int i = 0; i < mCurrentCategories.values().size(); i++) {
+                    choosedCategories[i] = (boolean) mCurrentCategories.values().toArray()[i];
+                }
+
+                builder.setMultiChoiceItems(categories, choosedCategories, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        List<String> keys = new ArrayList<>(mCurrentCategories.keySet());
+                        String filter = keys.get(which);
+                        mCurrentCategories.put(filter, isChecked);
+                        updateMarkers();
+                    }
+                });
+
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.show();
+            }
+        });
+    }
+
+    private void initPlacesList(View layout) {
+        mRecyclerPlaces = (RecyclerView) layout.findViewById(R.id.list_places);
+        List<Place> data = Storage.getInstance(getActivity().getApplicationContext()).getPlaces();
+        mAdapter = new PlacesAdapter(data, new PlacesAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Place place) {
+                moveCameraToPlace(place);
+                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        });
+        mRecyclerPlaces.setLayoutManager(new GridLayoutManager(getActivity().getApplicationContext(), 1));
+        mRecyclerPlaces.setAdapter(mAdapter);
+        mRecyclerPlaces.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
+    }
+
+    private void initBottomSheet(View layout) {
+        final View bottomSheet = layout.findViewById(R.id.view_bottom_sheet);
+        mBtnShowList = (Button) layout.findViewById(R.id.btn_show_list);
+        mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                mBtnShowList.setVisibility(newState == BottomSheetBehavior.STATE_HIDDEN ? View.VISIBLE : View.INVISIBLE);
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+        mBtnShowList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        });
+    }
+
+    private void moveCameraToPlace(Place place) {
+        LatLng latLng = new LatLng(place.getLat(), place.getLon());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(18));
+    }
+
+    private void updateMarkers() {
+        List<String> searchCategories = new ArrayList<>();
+        for(Map.Entry<String, Boolean> entry : mCurrentCategories.entrySet()) {
+            if(entry.getValue()) searchCategories.add(entry.getKey());
+        }
+        List<Place> data = Storage.getInstance(getActivity().getApplicationContext()).getPlacesWithCategories(searchCategories);
+        mAdapter.updateData(data);
+
+        mMap.clear();
+        for(Place place : data) {
+            LatLng latLng = new LatLng(place.getLat(), place.getLon());
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(latLng);
+            markerOptions.title(place.getName());
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+            mMap.addMarker(markerOptions);
+        }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 buildGoogleApiClient();
@@ -70,6 +214,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, Google
             mMap.setMyLocationEnabled(true);
         }
 
+        updateMarkers();
     }
 
     private synchronized void buildGoogleApiClient() {
@@ -85,19 +230,9 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, Google
     @Override
     public void onLocationChanged(Location location) {
         mLastLocation = location;
-        if (mCurrLocationMarker != null) {
-            mCurrLocationMarker.remove();
-        }
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
 
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(latLng);
-        markerOptions.title("Current position");
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
-
-        mCurrLocationMarker = mMap.addMarker(markerOptions);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
 
         if (mGoogleApiClient != null) {
             LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
